d409395a56db2e0568e8afaf135588c5536609ed
vyang <v.yang2968@gmail.com>
2025-09-27 16:00:03 -0400

    works locally...

--- stack/server.tsx
+++ stack/server.tsx
@@ -5,10 +5,12 @@ import { StackServerApp } from "@stackframe/stack";
 export const stackServerApp = new StackServerApp({
   tokenStore: "nextjs-cookie",
   urls: {
-    home: "/",
-    afterSignIn: "/",
-    afterSignUp: "/auth/sign-up/account-information",
-    signIn: "/auth/sign-in",
     signUp: "/auth/sign-up",
+    signIn: "/auth/sign-in",
+    afterSignUp: "/auth/sign-up/account-information",
+    afterSignIn: "/",
+    oauthCallback: "/handler/oauth-callback",
+    handler: "/handler",
+    home: "/",
   },
 });
