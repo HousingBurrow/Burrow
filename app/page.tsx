dcb5b30c9d50433a7a70d8c22228c9dc2ca81623
tracyyxie <127050708+tracyyxie@users.noreply.github.com>
2025-09-27 02:18:19 -0400

    listcard popup

--- app/page.tsx
+++ app/page.tsx
@@ -79,7 +79,7 @@ export const SearchBar: FC = () => {
 
      {/* Date Range */}
      <RangePicker
-       value={dates as any}
+       value={dates}
        onChange={(val) => setDates(val as [Dayjs, Dayjs])}
        style={{ borderRadius: 999 }}
      />
