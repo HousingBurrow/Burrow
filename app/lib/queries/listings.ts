05ebc48d233bde2feb5489c8ecf27aff06f11e36
jlee4103 <jlee4103@gatech.edu>
2025-09-27 02:09:58 -0400

    pelase

--- app/lib/queries/listings.ts
+++ app/lib/queries/listings.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { PrismaClient, type Listing, type PropertyType, type Location } from '@prisma/client';
+import { PrismaClient, Listing, PropertyType, Location } from '@prisma/client';
 import { ActionResult } from '../utils/action-result';
 import { prisma } from '../../../lib/prisma';
 import { Action } from '@prisma/client/runtime/library';
